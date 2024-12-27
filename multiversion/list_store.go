39a066d24645fe263a33614ad2e2cd142f61e331
jack <jack@everylink.ai>
2024-12-27 14:13:16 +0800

    add setIf for sharedMap

--- multiversion/list_store.go
+++ multiversion/list_store.go
@@ -599,6 +599,18 @@ func (sm *ShardedMap) Get(key string) (interface{}, bool) {
 	return value, exists
 }
 
+func (sm *ShardedMap) SetIf(key string, value interface{}, ifFun func(interface{}, bool) bool) interface{} {
+	shardIndex := sm.getShard(key)
+	sm.locks[shardIndex].RLock()
+	defer sm.locks[shardIndex].RUnlock()
+	old, exists := sm.shards[shardIndex][key]
+	if ifFun(old, exists) {
+		sm.shards[shardIndex][key] = value
+		return value
+	}
+	return old
+}
+
 func (sm *ShardedMap) Delete(key string) bool {
 	shardIndex := sm.getShard(key)
 	sm.locks[shardIndex].Lock()
